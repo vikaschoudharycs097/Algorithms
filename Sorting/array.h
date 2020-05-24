edb642a040a70d2689115d57d1efac82bed9f239
vikaschoudharycs097 <vikaschoudharycs097@gmail.com>
2020-05-25 00:00:31 +0530

    Feat: Adding isSorted() to array.h

--- Sorting/array.h
+++ Sorting/array.h
@@ -41,4 +41,19 @@ void printArray(T arr[], int n, int m)
     }
 }
 
+// Check that array is sorted or not
+template<typename T>
+bool isSorted(T arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 #endif
