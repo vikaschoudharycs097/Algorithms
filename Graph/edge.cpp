765d7a50f60fe914e350264eef865b62c86f9c32
vikaschoudharycs097 <vikaschoudharycs097@gmail.com>
2020-06-01 23:28:45 +0530

    Feat: Adding Edge class interface

---
+++ Graph/edge.cpp
@@ -0,0 +1,37 @@
+/************************************
+ * Implementation of Edge class
+ * 
+ * @author Vikas Choudhary
+ * @date 01.06.2020
+ ************************************/
+
+#include "edge.h"
+
+// Constructor
+Edge::Edge(int src_vertex, int dest_vertex):
+_src_vertex(src_vertex), _dest_vertex(dest_vertex)
+{
+
+}
+
+Edge::Edge(int src_vertex, int dest_vertex, double weight):
+_src_vertex(src_vertex), _dest_vertex(dest_vertex), _weight(weight)
+{
+
+}
+
+// Getters
+int Edge::getSource(void) const
+{
+    return _src_vertex;
+}
+
+int Edge::getDest(void) const
+{
+    return _dest_vertex;
+}
+
+int Edge::getWeight(void) const
+{
+    return _weight;
+}
