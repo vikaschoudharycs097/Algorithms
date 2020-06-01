67ab7c938f98ae9ac82fd792597b55883dc2f6ce
vikaschoudharycs097 <vikaschoudharycs097@gmail.com>
2020-06-01 23:15:33 +0530

    Feat: Adding Edge class interface

---
+++ Graph/edge.h
@@ -0,0 +1,23 @@
+/********************************************
+ * Implementation of Edge class
+ * 
+ * @author Vikas Choudhary
+ * @date 01.06.2020
+ *******************************************/
+
+// Representing an edge in a Graph
+class Edge 
+{
+    int _src_vertex;
+    int _dest_vertex;
+    double weight;
+public:
+    // Constructor
+    Edge(int src_vertex, int dest_vertex);
+    Edge(int src_vertex, int dest_vertex, double weight);
+
+    // Getters
+    int getSource(void) const;
+    int getDest(void) const;
+    int getWeight(void) const;
+};
