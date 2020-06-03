7d2e4833e7daedec0a2058f2265a8f2d447a692b
vikaschoudharycs097 <vikaschoudharycs097@gmail.com>
2020-06-03 19:57:25 +0530

    Feat: 'addEdge' function implementation Body: Implementation of 'addEdge' member function in class AdjancyList

--- Graph/adjancylist.cpp
+++ Graph/adjancylist.cpp
@@ -6,6 +6,7 @@
  ***************************************/
 
 #include "adjancylist.h"
+#include <iostream>
 
 // Constructors
 AdjancyList::AdjancyList(): _list(0)
@@ -28,4 +29,30 @@ int AdjancyList::getNumOfVertex(void) const
 void AdjancyList::setNumOfVertex(int n_vertex)
 {
     _list.resize(n_vertex);
+}
+
+void AdjancyList::addEdge(int src, int dest)
+{
+    if (src < 0 || src > _list.size() || dest < 0 || dest > _list.size())
+    {
+        std::cerr << "Error: Invalid source or destination vertex\n";
+    }
+    else 
+    {
+        EdgeNode *edge = new EdgeNode(src, dest, _list[src]);
+        _list[src] = edge; 
+    }
+}
+
+void AdjancyList::addEdge(int src, int dest, double weight)
+{
+    if (src < 0 || src > _list.size() || dest < 0 || dest > _list.size())
+    {
+        std::cerr << "Error: Invalid source or destination vertex\n";
+    }
+    else 
+    {
+        EdgeNode *edge = new EdgeNode(src, dest, weight, _list[src]);
+        _list[src] = edge; 
+    }
 }
