9267536519dffcf14bc8ae64de0f68211caa7fef
vikaschoudharycs097 <vikaschoudharycs097@gmail.com>
2020-06-05 22:39:41 +0530

    Feat: Printing n by n square

---
+++ Pattern/square.cpp
@@ -0,0 +1,48 @@
+/*****************************************
+ * Printing n '*' per line in n lines
+ * 
+ * @author Vikas Choudhary
+ * @date 05-06-2020
+ ****************************************/
+
+#include <iostream>
+
+// Function prototypes
+int getPositive(void);
+
+int main(void)
+{
+    int n;
+
+    std::cout << "Enter a positive integer: ";
+    n = getPositive();
+
+    // Displaying pattern
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            std::cout << "* ";
+        }
+        std::cout << "\n";
+    }
+
+    return 0;
+}
+
+/**
+ * Reading a positive integer
+ * @return: Returning a positive integer
+ */
+int getPositive(void)
+{
+    int n;
+    std::cin >> n;
+    while (n < 1)
+    {
+        std::cout << "Retry: ";
+        std::cin >> n;
+    } 
+
+    return n;
+} 
