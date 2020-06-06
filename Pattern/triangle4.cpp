0a1a4e5956357a5f3f617ca09be2525e878773cd
vikaschoudharycs097 <vikaschoudharycs097@gmail.com>
2020-06-06 20:12:40 +0530

    Feat: Right angled inverted right aligned triangle

---
+++ Pattern/triangle4.cpp
@@ -0,0 +1,55 @@
+/*****************************************
+ * Printing right angle triangle inverted
+ * aligned right side
+ * 
+ * @author Vikas Choudhary
+ * @date 05-06-2020
+ ****************************************/
+
+#include <iostream>
+
+// Function prototypes
+int getPositive(void);
+
+int main(void)
+{
+    int n;
+
+    std::cout << "Enter a positive integer: ";
+    n = getPositive();
+
+    // Displaying pattern
+    for (int i = 0; i < n; i++)
+    {
+        // Inserting space to make right aligned
+        for (int j = 0; j < i; j++)
+        {
+            std::cout << "  ";
+        }
+
+        for (int j = n; j > i; j--)
+        {
+            std::cout << "* ";
+        }
+        std::cout << "\n";
+    }
+
+    return 0;
+}
+
+/**
+ * Reading a positive integer
+ * @return: Returning a positive integer
+ */
+int getPositive(void)
+{
+    int n;
+    std::cin >> n;
+    while (n < 1)
+    {
+        std::cout << "Retry: ";
+        std::cin >> n;
+    } 
+
+    return n;
+} 
