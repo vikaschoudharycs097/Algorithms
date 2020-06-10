fd4ccc32601005bfa76d7205e87225a2b4fd58a4
vikaschoudharycs097 <vikaschoudharycs097@gmail.com>
2020-06-10 23:12:36 +0530

    Feat: Adding interface of Matrix class

---
+++ Math/matrix.h
@@ -0,0 +1,21 @@
+/***************************************
+ * Implementation of Matrix class
+ * 
+ * @author Vikas Choudhary
+ * @date 10.06.2020
+ **************************************/
+#include <vector>
+
+template<typename T>
+class Matrix
+{
+private:
+    int _rows;
+    int _cols;
+    T **_matrix;
+
+public:
+    Matrix(int rows, int cols);
+    Matrix(int rows, int cols, std::vector<std::vector<int>> &data);
+    Matrix(const Matrix &mat);
+};
