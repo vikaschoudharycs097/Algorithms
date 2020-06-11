b5e8828b13fc221a58a0281b406671c9262c011c
vikaschoudharycs097 <vikaschoudharycs097@gmail.com>
2020-06-11 23:11:48 +0530

    Feat: Implementation of Matrix class

---
+++ Math/matrix.cpp
@@ -0,0 +1,97 @@
+/***************************************
+ * Implementation of Matrix class
+ * 
+ * @author Vikas Choudhary
+ * @date 10.06.2020
+ **************************************/
+
+#include "matrix.h"
+#include <iostream>
+
+// Constructors
+template<typename T>
+Matrix<T>::Matrix(int rows, int cols): _rows(rows), _cols(cols)
+{
+    _matrix = new T*[rows];
+    for (int i = 0; i < rows; i++)
+    {
+        _matrix[i] = new T[cols];
+    }
+}
+
+template<typename T>
+Matrix<T>::Matrix(int rows, int cols, std::vector<std::vector<T>> &data):
+_rows(rows), _cols(cols)
+{
+    _matrix = new T*[rows];
+    for (int i = 0; i < rows; i++)
+    {
+        _matrix[i] = new T[cols];
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; i++)
+        {
+            _matrix[i][j] = data[i][j];
+        }
+    }
+}
+
+template<typename T>
+Matrix<T>::Matrix(const Matrix &mat): _rows(mat._rows), _cols(mat._cols)
+{
+    _matrix = new T*[_rows];
+    for (int i = 0; i < _rows; i++)
+    {
+        _matrix[i] = new T[_cols];
+    }
+
+    for (int i = 0; i < _rows; i++)
+    {
+        for (int j = 0; j < _cols; i++)
+        {
+            _matrix[i][j] = mat._matrix[i][j];
+        }
+    }
+}
+
+// Input matrix
+template<typename T>
+void Matrix<T>::input(void)
+{
+    for (int i = 0; i < _rows; i++)
+    {
+        for (int j = 0; j < _cols; j++)
+        {
+            std::cin >> _matrix[i][j];
+        }
+    }
+}
+
+// Output matrix
+template<typename T>
+void Matrix<T>::print(void)
+{
+    for (int i = 0; i < _rows; i++)
+    {
+        for (int j = 0; j < _cols; j++)
+        {
+            std::cout << _matrix[i][j] << "\t";
+        }
+        printf("\n");
+    }
+}
+
+// Getters
+template<typename T>
+int Matrix<T>::getRows(void) const
+{
+    return _rows;
+}
+
+template<typename T>
+int Matrix<T>::getCols(void) const
+{
+    return _cols;
+}
